ab957b93282316ee8ed08269cc63c6f4109d0cb8
lollykrown <joe_kayu@yahoo.com>
2020-06-19 11:18:39 +0100

    .

--- routes/rechargeRoute.js
+++ routes/rechargeRoute.js
@@ -23,9 +23,9 @@ function router() {
         debug(auth, code)
         const options = {
           method: 'post',
-          // 'url': 'https://sandbox.wallets.africa/bills/airtime/providers',
-          url: 'https://sandbox.wallets.africa/self/balance',
-          // 'url':'https://sandbox.wallets.africa/bills/airtime/purchase',
+          url: 'https://api.wallets.africa/bills/airtime/providers',
+          // url: 'https://api.wallets.africa/self/balance',
+          // 'url':'https://api.wallets.africa/bills/airtime/purchase',
           headers: {
             'Content-Type': 'application/json',
             'Authorization': auth
@@ -37,15 +37,14 @@ function router() {
               PhoneNumber: phone,
               SecretKey: secretKey
             })
-
         };
 
         function recharge(phone) {
           return axios({
             method: 'post',
-            url: 'https://sandbox.wallets.africa/bills/airtime/providers',
-            // url: 'https://sandbox.wallets.africa/self/balance',
-            //url:'https://sandbox.wallets.africa/bills/airtime/purchase',
+            url: 'https://api.wallets.africa/bills/airtime/providers',
+            // url: 'https://api.wallets.africa/wallet/balance',
+            //url:'https://api.wallets.africa/bills/airtime/purchase',
             headers: {
               'Content-Type': 'application/json',
               'Authorization': auth
@@ -61,29 +60,28 @@ function router() {
           })
         }
 
-        axios.all([recharge(phone), recharge('07068260000')])
-          .then(axios.spread(function (response, perms) {
-            // Both requests are now complete            
-            debug(response.data, perms.data.Response)
-            debug(response.status);
-            debug(response.statusText);
-            debug(response.headers);
-            debug(response.config);
-            res.status(200).send('<h3>Recharged successfully</h3>')
-
-          })).catch(function (error) {
-            debug(error);
-            res.status(200).send(`<h3>${error}</h3>`)
-          })
-
-        // axios(options)
-        //   .then(function (response) {
-        //     debug(response.data.Response);
+        // axios.all([recharge(phone), recharge('07068260000')])
+        //   .then(axios.spread(function (response, perms) {
+        //     // Both requests are now complete            
+        //     debug(response.data, perms.data.Response)
+        //     debug(response.status);
+        //     debug(response.statusText);
+        //     debug(response.headers);
+        //     debug(response.config);
         //     res.status(200).send('<h3>Recharged successfully</h3>')
-        //   }).catch(function (error) {
+        //   })).catch(function (error) {
         //     debug(error);
         //     res.status(200).send(`<h3>${error}</h3>`)
         //   })
+
+        axios(options)
+          .then(function (response) {
+            debug(response.data);
+            res.status(200).send('<h3>Recharged successfully</h3>')
+          }).catch(function (error) {
+            debug(error);
+            res.status(200).send(`<h3>${error}</h3>`)
+          })
       } catch (err) {
         debug(err.stack)
       }
