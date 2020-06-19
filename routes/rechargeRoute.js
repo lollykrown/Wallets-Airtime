5231563c6585dba29bd778777e0208c242af51bf
lollykrown <joe_kayu@yahoo.com>
2020-06-19 19:55:17 +0100

    multiple

--- routes/rechargeRoute.js
+++ routes/rechargeRoute.js
@@ -39,27 +39,6 @@ function router() {
             })
         };
 
-        function recharge(phone) {
-          return axios({
-            method: 'post',
-            //url: 'https://api.wallets.africa/bills/airtime/providers',
-            // url: 'https://api.wallets.africa/wallet/balance',
-            //url:'https://api.wallets.africa/bills/airtime/purchase',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': auth
-            },
-            data: JSON.stringify(
-              {
-                Code: code,
-                Amount: 100,
-                PhoneNumber: phone,
-                SecretKey: secretKey
-              })
-  
-          })
-        }
-
         // axios.all([recharge(phone), recharge('07068260000')])
         //   .then(axios.spread(function (response, perms) {
         //     // Both requests are now complete            
@@ -84,6 +63,63 @@ function router() {
           })
       } catch (err) {
         debug(err.stack)
+      }
+    }());
+  })
+
+  rechargeRoute.route('/multiple').post((req, res) => {
+    (async function postCards() {
+      try {
+
+        function recharge(phone, network, amount) {
+          return axios({
+            method: 'post',
+            url: 'https://api.wallets.africa/bills/airtime/providers',
+            // url: 'https://api.wallets.africa/wallet/balance',
+            //url:'https://api.wallets.africa/bills/airtime/purchase',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer 103lamgop7db'
+            },
+            data: JSON.stringify(
+              {
+                Code: network,
+                Amount: amount,
+                PhoneNumber: phone,
+                SecretKey: '5u5aak9rr9ke'
+              })
+  
+          })
+        }
+        let arr = req.body
+        debug(arr)
+        let response = [] 
+        for(let i of arr){
+          let number = i.phone
+          let code = i.network.trim().toLowerCase()
+          let amount = i.amount
+          debug(number, code, amount)
+          const resu = await recharge(number, code, amount)
+          //response.push(resu.data)
+          response.push(resu.data)
+        }
+        debug(response)
+        noOfSuccessfulRecharges = 0
+        noOfFailedRecharges = 0
+        for(let i of response){
+          if(i === '100'){
+            noOfSuccessfulRecharges =+ 1
+          }
+          if(i !== '100'){
+            noOfFailedRecharges =+ 1
+          }
+        }
+        res.status(200).json(response)
+        // res.status(200).render('view', {
+        //   msg: `${noOfSuccessfulRecharges} were successful while ${noOfFailedRecharges} were not`})
+      } catch (error) {
+        debug(error.stack)
+        res.status(200).render('error', {err: error.stack})
       }
     }());
   })
